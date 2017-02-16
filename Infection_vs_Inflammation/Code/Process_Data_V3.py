cd25a048554052e035130df6f3b12c8162ae7fe4
Julio Cardenas <cardenaj@email.arizona.edu>
2017-02-16 14:22:34 -0700

    Process Data version 3.0

--- Infection_vs_Inflammation/Code/Process_Data_V3.py
+++ Infection_vs_Inflammation/Code/Process_Data_V3.py
@@ -1,58 +1,16 @@
 # Import Modules as needed
 import numpy as np
-#import seaborn as sn
-import pandas as pd
 from mylocal_functions import *
 import matplotlib.pyplot as plt
 
 # ======== CEST============= #
-# Make list of all CEST.txt files
 CEST_list=get_ipython().getoutput('ls ../Study_03_CBA/*CEST.txt')
+CEST_Int_matrix=np.zeros((len(CEST_list),4))
 
-# create function to normalize data
-def normalize_data(DataMatrix,normalization_point):
-    rows,cols = DataMatrix.shape
-    newData = np.zeros_like(DataMatrix)
-    for row in range(rows):
-        newData[row,:]=DataMatrix[row,:]/DataMatrix[row,normalization_point]
-    return newData
-
-# extract CEST data as a 4 X 110 matrix
-Z=np.zeros((4,110))
-for names in CEST_list:
-    D=txt_2_array(names);       #Convert txt file to array
-    Zn=normalize_data(D.T)
-    Z=np.concatenate((Z,Zn))
-Z=Z[4::,9::]
-
-# define offsets in ppm
-a1=np.linspace(-55,-50,9);  ppm=np.linspace(-8,8,101);  full_ppm = np.concatenate((a1, ppm))
-
-
-# Fit data to center it
-CEST_centered=np.zeros_like(Z); rows,cols = CEST_centered.shape;
-CEST_integral=np.zeros((rows,1))
-
-for i in range(rows):
-    p=fit_L2_scale(ppm,Z[i,:])
-    L=Lscale(ppm,p[0],p[1],p[2],p[3],p[4],p[5],p[6]);
-    CEST_centered[i,:]=L
-    CEST_integral[i,0]=np.sum(L)
-# create tissue label    
-TissueLabel=np.zeros((rows,1))
-for i in range(1,4):
-    TissueLabel[i::4]=i
-
-CEST_integral_df= pd.DataFrame(data=CEST_integral,columns=["CEST_integral"]); 
-CEST_integral_df["Tissue"]=TissueLabel
-
-Y=np.zeros((16,4))
-for i in range(4):
-    df=CEST_integral_df[CEST_integral_df["Tissue"]==i]
-    Y[:,i]=df.CEST_integral.values;
-    
-
-
+for i in range( len(CEST_list) ):
+    D=txt_2_array(CEST_list[i]);       #Convert txt file to array
+    Zn=normalize_data(D.T,8);          Zn=Zn[:,9::]
+    CEST_Int_matrix[i,:]=np.sum(Zn,axis=1) 
 
 # ======== T2 MSME============= #
 # Make list of all T2.txt files
@@ -67,17 +25,36 @@ for i in range(len(T2_list)):
     #Estimate T2
     T2time=fitT2(TR,YDataMatrix)
     T2_matrix[i,:]=T2time.T
-    
+
+# ======== T2ex DCE============= #
+# Make list of all T2.txt files
+T2ex_list = get_ipython().getoutput('ls ../Study_03_CBA/*S3*T2exDCE.txt')
+T2ex_Int_matrix=np.zeros( (len(T2ex_list),4) )
+
+# T2ex integral
+for i in range( len(T2ex_list) ):
+    D=txt_2_array(T2ex_list[i]);       #Convert txt file to array
+    Zn=normalize_data(D.T,0);          #Zn=Zn[:,9::]
+    T2ex_Int_matrix[i,:]=np.sum(Zn-1,axis=1)
+
+
 #======== create violing plots ============= #
 Tissues=["Infected","Healthy R","Sterile Infl.","Healthy K"]
-fig = plt.figure(); ax = fig.add_subplot(111)
-ax.set_xticks([1, 2, 3, 4])
-ax.set_xticklabels(Tissues)
-plt.violinplot(Y); plt.ylabel("CEST Integral")
 
-# create violing plot
-fig = plt.figure(); ax = fig.add_subplot(111)
-ax.set_xticks([1, 2, 3, 4])
-ax.set_xticklabels(Tissues)
-plt.violinplot(T2_matrix);  plt.ylabel("T2 time")
+# Set dimensions of plot
+fig = plt.figure(10,figsize=(10,10));
+# CEST
+ax = fig.add_subplot(3,1,1);  ax.set_xticks([1, 2, 3, 4]);  ax.set_xticklabels(Tissues)
+plt.violinplot(CEST_Int_matrix, showextrema=True,showmedians=True);
+plt.ylabel("CEST Integral")
+#T2
+ax = fig.add_subplot(3,1,2);  ax.set_xticks([1, 2, 3, 4]);  ax.set_xticklabels(Tissues)
+plt.violinplot(T2_matrix,showextrema=True,showmedians=True);
+plt.ylabel("T2 time")
+#T2ex
+ax = fig.add_subplot(3,1,3);  ax.set_xticks([1, 2, 3, 4]);  ax.set_xticklabels(Tissues)
+plt.violinplot(T2ex_Int_matrix,showextrema=True,showmedians=True);
+plt.ylabel("T2ex Integral")
 
+p=plt.violinplot(T2ex_Int_matrix,showextrema=True,showmedians=True);
+plt.ylabel("T2ex Integral")
